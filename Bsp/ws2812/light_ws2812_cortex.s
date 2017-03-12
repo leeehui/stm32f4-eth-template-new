8a56994bd2bc236882eac981a496d8d16cf72037
lenli <leehui_hrbeu71102@163.com>
2017-03-12 20:08:33 +0800

    add all files

---
+++ Bsp/ws2812/light_ws2812_cortex.s
@@ -0,0 +1,328 @@
+
+#include "light_ws2812_conf.h"
+	//RSEG    CODE:CODE(2)
+        ;; Forward declaration of sections.
+	PUBLIC ws2812_send
+
+
+        SECTION .text:CODE:REORDER:NOROOT(2)
+        thumb
+ws2812_send:
+        stmfd sp,{r4-r11}
+        ldr r4, [sp]          ;*data4
+        ldr r5, [sp, #4]      ;*data5  
+        ldr r6, [sp, #8]     ;len
+        
+        mov r8, #0x80           ;counter i
+byte_loop:
+        
+        mov r9, #0              ;flag
+        //mov32 r11, #0x40200418  ;use portB for all channels, do not need to set inside the loop
+       
+        //calculate flag
+        ldr r7, [r0]            ;data0
+        tst r7, r8
+        it eq
+        orreq r9, r9, #0x01
+        
+        ldr r7, [r1]            ;data1
+        tst r7, r8
+        it eq
+        orreq r9, r9, #0x02
+        
+        ldr r7, [r2]            ;data2
+        tst r7, r8
+        it eq
+        orreq r9, r9, #0x04
+        
+        ldr r7, [r3]            ;data3
+        tst r7, r8
+        it eq
+        orreq r9, r9, #0x08
+
+        ldr r7, [r4]            ;data4
+        tst r7, r8
+        it eq
+        orreq r9, r9, #0x10
+        
+        ldr r7, [r5]            ;data5
+        tst r7, r8
+        it eq
+        orreq r9, r9, #0x20
+        
+        //T0(T3) : set output high
+        ldr r11, =port_set_ch_1  ;all channels use same port
+        
+        ldr r10, =pin_mask_ch_1
+        str r10, [r11]
+        
+        //ldr r11, =port_set_ch_2
+        ldr r10, =pin_mask_ch_2
+        str r10, [r11]
+        
+        //ldr r11, =port_set_ch_2
+        ldr r10, =pin_mask_ch_2
+        str r10, [r11]
+
+        //ldr r11, =port_set_ch_3
+        ldr r10, =pin_mask_ch_3
+        str r10, [r11]
+        
+        //ldr r11, =port_set_ch_4
+        ldr r10, =pin_mask_ch_4
+        str r10, [r11]
+        
+        //ldr r11, =port_set_ch_5
+        ldr r10, =pin_mask_ch_5
+        str r10, [r11]
+
+        //ldr r11, =port_set_ch_6
+        ldr r10, =pin_mask_ch_6
+        str r10, [r11]
+        nop
+        nop
+        nop
+        nop
+        nop
+        
+        nop
+        nop
+        nop
+        nop
+        nop
+        
+        nop
+        nop
+        nop
+        nop
+        nop
+
+        nop
+        nop
+        nop
+        nop
+        nop
+
+        nop
+        nop
+        nop
+        nop
+        nop
+
+        nop
+        nop
+        nop
+        nop
+        nop
+        
+        nop
+        nop
+        nop
+        nop
+        nop
+
+
+/* 
+        ldr r12, =w1_multi
+delay_1:
+        subs r12, r12, #1
+        bne delay_1
+*/
+/*
+#if (w1_multi&1)                     //nop delay
+			ws2812_DEL1
+#endif
+#if (w1_multi&2)
+			ws2812_DEL2
+#endif
+#if (w1_multi&4)
+			ws2812_DEL4
+#endif
+#if (w1_multi&8)
+			ws2812_DEL8
+#endif
+#if (w1_multi&16)
+			ws2812_DEL16
+#endif
+#if (w1_multi&32)
+			ws2812_DEL32
+#endif
+#if (w1_multi&64)
+			ws2812_DEL64
+#endif
+#if (w1_multi&128)
+			ws2812_DEL128
+#endif
+*/
+        ldr r11, =port_clr_ch_1     ;all channels use same port
+        
+        tst r9, #0x01
+        itt ne 
+        ldrne r10, =pin_mask_ch_1
+        strne r10, [r11]
+        
+        tst r9, #0x02
+        itt ne 
+        ldrne r10, =pin_mask_ch_2
+        strne r10, [r11]
+
+        tst r9, #0x04
+        itt ne 
+        ldrne r10, =pin_mask_ch_3
+        strne r10, [r11]
+
+        tst r9, #0x08
+        itt ne 
+        ldrne r10, =pin_mask_ch_4
+        strne r10, [r11]
+        
+        tst r9, #0x10
+        itt ne 
+        ldrne r10, =pin_mask_ch_5
+        strne r10, [r11]
+
+        tst r9, #0x20
+        itt ne 
+        ldrne r10, =pin_mask_ch_6
+        strne r10, [r11]
+        
+        nop
+        nop
+        nop
+        nop
+        nop
+        
+        nop
+        nop
+        nop
+        nop
+        nop
+
+        nop
+        nop
+        nop
+
+ /*
+        ldr r12, =w2_multi
+delay_2:
+        subs r12, r12, #1
+        bne delay_2
+*/
+/*
+#if (w2_multi&1)                            //nop delay
+			ws2812_DEL1
+#endif
+#if (w2_multi&2)
+			ws2812_DEL2
+#endif
+#if (w2_multi&4)
+			ws2812_DEL4
+#endif
+#if (w2_multi&8)
+			ws2812_DEL8
+#endif
+#if (w2_multi&16)
+			ws2812_DEL16
+#endif
+#if (w2_multi&32)
+			ws2812_DEL32
+#endif
+#if (w2_multi&64)
+			ws2812_DEL64
+#endif
+#if (w2_multi&128)
+			ws2812_DEL128
+#endif
+*/
+      
+        //T2 : unconditionally set output low
+        ldr r11, =port_clr_ch_1  ;all channels use same port
+        
+        ldr r10, =pin_mask_ch_1
+        str r10, [r11]
+        
+        //ldr r11, =port_set_ch_2
+        ldr r10, =pin_mask_ch_2
+        str r10, [r11]
+        
+        //ldr r11, =port_set_ch_2
+        ldr r10, =pin_mask_ch_2
+        str r10, [r11]
+
+        //ldr r11, =port_set_ch_3
+        ldr r10, =pin_mask_ch_3
+        str r10, [r11]
+        
+        //ldr r11, =port_set_ch_4
+        ldr r10, =pin_mask_ch_4
+        str r10, [r11]
+        
+        //ldr r11, =port_set_ch_5
+        ldr r10, =pin_mask_ch_5
+        str r10, [r11]
+
+        //ldr r11, =port_set_ch_6
+        ldr r10, =pin_mask_ch_6
+        str r10, [r11]
+
+        nop
+        nop
+        nop
+        nop
+        nop
+        nop
+        nop
+        nop
+
+
+/*
+        ldr r12, =w3_multi
+delay_3:
+        subs r12, r12, #1
+        bne delay_3
+*/
+/*  
+#if (w3_multi&1)
+			ws2812_DEL1
+#endif
+#if (w3_multi&2)
+			ws2812_DEL2
+#endif
+#if (w3_multi&4)
+			ws2812_DEL4
+#endif
+#if (w3_multi&8)
+			ws2812_DEL8
+#endif
+#if (w3_multi&16)
+			ws2812_DEL16
+#endif
+#if (w3_multi&32)
+			ws2812_DEL32
+#endif
+#if (w3_multi&64)
+			ws2812_DEL64
+#endif
+#if (w3_multi&128)
+			ws2812_DEL128
+#endif
+*/  
+        lsrs r8, r8, #1
+        beq byte_end
+        b byte_loop
+byte_end:
+        subs r6, r6, #1
+        beq all_end
+        add r0, r0, #1    
+        add r1, r1, #1
+        add r2, r2, #1
+        add r3, r3, #1
+        add r4, r4, #1
+        add r5, r5, #1
+        mov r8, #0x80           ;counter i
+        b byte_loop
+all_end:    
+        sub r0, sp, #32
+        ldmfd r0,{r4-r11}
+        mov pc, lr
+        END
---
+++ Src/lwip.c
@@ -0,0 +1,222 @@
+/**
+ ******************************************************************************
+  * File Name          : LWIP.c
+  * Description        : This file provides initialization code for LWIP
+  *                      middleWare.
+  ******************************************************************************
+  *
+  * Copyright (c) 2017 STMicroelectronics International N.V. 
+  * All rights reserved.
+  *
+  * Redistribution and use in source and binary forms, with or without 
+  * modification, are permitted, provided that the following conditions are met:
+  *
+  * 1. Redistribution of source code must retain the above copyright notice, 
+  *    this list of conditions and the following disclaimer.
+  * 2. Redistributions in binary form must reproduce the above copyright notice,
+  *    this list of conditions and the following disclaimer in the documentation
+  *    and/or other materials provided with the distribution.
+  * 3. Neither the name of STMicroelectronics nor the names of other 
+  *    contributors to this software may be used to endorse or promote products 
+  *    derived from this software without specific written permission.
+  * 4. This software, including modifications and/or derivative works of this 
+  *    software, must execute solely and exclusively on microcontroller or
+  *    microprocessor devices manufactured by or for STMicroelectronics.
+  * 5. Redistribution and use of this software other than as permitted under 
+  *    this license is void and will automatically terminate your rights under 
+  *    this license. 
+  *
+  * THIS SOFTWARE IS PROVIDED BY STMICROELECTRONICS AND CONTRIBUTORS "AS IS" 
+  * AND ANY EXPRESS, IMPLIED OR STATUTORY WARRANTIES, INCLUDING, BUT NOT 
+  * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
+  * PARTICULAR PURPOSE AND NON-INFRINGEMENT OF THIRD PARTY INTELLECTUAL PROPERTY
+  * RIGHTS ARE DISCLAIMED TO THE FULLEST EXTENT PERMITTED BY LAW. IN NO EVENT 
+  * SHALL STMICROELECTRONICS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+  * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, 
+  * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF 
+  * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING 
+  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+  * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  *
+  ******************************************************************************
+  */
+  
+/* Includes ------------------------------------------------------------------*/
+#include "lwip.h"
+#include "lwip/init.h"
+#include "lwip/netif.h"
+#include "ethernetif.h"
+#include "lwip/api.h"
+
+
+
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/* ETH Variables initialization ----------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN 1 */
+
+/* USER CODE END 1 */
+/* Semaphore to signal Ethernet Link state update */
+osSemaphoreId Netif_LinkSemaphore = NULL;
+/* Ethernet link thread Argument */
+struct link_str link_arg;
+
+/* Variables Initialization */
+struct netif gnetif;
+ip4_addr_t ipaddr;
+ip4_addr_t netmask;
+ip4_addr_t gw;
+uint8_t IP_ADDRESS[4];
+uint8_t NETMASK_ADDRESS[4];
+uint8_t GATEWAY_ADDRESS[4];
+
+/* USER CODE BEGIN 2 */
+static void tcpecho_thread(void *arg)
+{
+	struct netconn *conn, *newconn;
+	err_t err, accept_err;
+	struct netbuf *buf;
+	void *data;
+	u16_t len;
+	err_t recv_err;
+	LWIP_UNUSED_ARG(arg);
+	/* Create a new connection identifier. */
+	conn = netconn_new(NETCONN_TCP);
+	if (conn!=NULL)
+	{
+		/* Bind connection to well known port number 7. */
+		err = netconn_bind(conn, NULL, 3004);
+		if (err == ERR_OK)
+		{
+			ip_addr_t ipaddr;
+			IP4_ADDR(&ipaddr, 192,168,0,9);
+			netconn_connect(conn, &ipaddr,6000);
+			printf("conneted\n");
+                        __set_PRIMASK(1);
+                        while(1)
+                        {
+                          test();
+                        }
+			/* Tell connection to go into listening mode. */
+//			netconn_listen(conn);
+			while (1)
+			{
+				recv_err = netconn_recv(conn, &buf);
+				
+				if(recv_err == ERR_OK)
+				{
+					printf("received data\n");
+					do
+					{
+					netbuf_data(buf, &data, &len);
+					netconn_write(conn, data, len, NETCONN_COPY);
+					}
+					while (netbuf_next(buf) >= 0);
+					netbuf_delete(buf);
+				}
+				/* Grab new connection. */
+//				accept_err = netconn_accept(conn, &newconn);
+				/* Process the new connection. */
+//				if (accept_err == ERR_OK)
+//				{
+//					while (( recv_err = netconn_recv(newconn, &buf)) == ERR_OK)
+//					{
+//						do
+//						{
+//						netbuf_data(buf, &data, &len);
+//						netconn_write(newconn, data, len, NETCONN_COPY);
+//						}
+//						while (netbuf_next(buf) >= 0);
+//						netbuf_delete(buf);
+//					}
+//					/* Close connection and discard connection identifier. */
+//					netconn_close(newconn);
+//					netconn_delete(newconn);
+//				}
+			}
+		}
+		else
+		{
+			netconn_delete(newconn);
+			printf(" can not bind TCP netconn");
+		}
+	}
+	else
+	{
+		printf("can not create TCP netconn");
+	}
+}
+/* USER CODE END 2 */
+
+/**
+  * LwIP initialization function
+  */
+void MX_LWIP_Init(void)
+{
+  /* IP addresses initialization */
+  IP_ADDRESS[0] = 192;
+  IP_ADDRESS[1] = 168;
+  IP_ADDRESS[2] = 0;
+  IP_ADDRESS[3] = 122;
+  NETMASK_ADDRESS[0] = 255;
+  NETMASK_ADDRESS[1] = 255;
+  NETMASK_ADDRESS[2] = 255;
+  NETMASK_ADDRESS[3] = 0;
+  GATEWAY_ADDRESS[0] = 192;
+  GATEWAY_ADDRESS[1] = 168;
+  GATEWAY_ADDRESS[2] = 0;
+  GATEWAY_ADDRESS[3] = 1;
+  
+  /* Initilialize the LwIP stack with RTOS */
+  tcpip_init( NULL, NULL );
+
+  /* IP addresses initialization without DHCP (IPv4) */
+  IP4_ADDR(&ipaddr, IP_ADDRESS[0], IP_ADDRESS[1], IP_ADDRESS[2], IP_ADDRESS[3]);
+  IP4_ADDR(&netmask, NETMASK_ADDRESS[0], NETMASK_ADDRESS[1] , NETMASK_ADDRESS[2], NETMASK_ADDRESS[3]);
+  IP4_ADDR(&gw, GATEWAY_ADDRESS[0], GATEWAY_ADDRESS[1], GATEWAY_ADDRESS[2], GATEWAY_ADDRESS[3]);
+
+  /* add the network interface (IPv4/IPv6) with RTOS */
+  netif_add(&gnetif, &ipaddr, &netmask, &gw, NULL, &ethernetif_init, &tcpip_input);
+
+  /* Registers the default network interface */
+  netif_set_default(&gnetif);
+
+  if (netif_is_link_up(&gnetif))
+  {
+    /* When the netif is fully configured this function must be called */
+    netif_set_up(&gnetif);
+  }
+  else
+  {
+    /* When the netif link is down this function must be called */
+    netif_set_down(&gnetif);
+  }
+
+  /* Set the link callback function, this function is called on change of link status*/
+  netif_set_link_callback(&gnetif, ethernetif_update_config);
+
+  /* create a binary semaphore used for informing ethernetif of frame reception */
+  osSemaphoreDef(Netif_SEM);
+  Netif_LinkSemaphore = osSemaphoreCreate(osSemaphore(Netif_SEM) , 1 );
+
+  link_arg.netif = &gnetif;
+  link_arg.semaphore = Netif_LinkSemaphore;
+  /* Create the Ethernet link handler thread */
+  osThreadDef(LinkThr, ethernetif_set_link, osPriorityNormal, 0, configMINIMAL_STACK_SIZE * 2);
+  osThreadCreate (osThread(LinkThr), &link_arg);
+	
+  sys_thread_new("tcpecho_thread", tcpecho_thread, NULL, DEFAULT_THREAD_STACKSIZE, osPriorityNormal);
+/* USER CODE BEGIN 3 */
+
+/* USER CODE END 3 */
+}
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
